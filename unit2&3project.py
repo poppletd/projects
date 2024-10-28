75fd077159fb68ea14e4a4de4bdd03abbce06e7f
Delaina Poppleton <poppletd@wlhs.wlwv.k12.or.us>
2024-10-28 10:12:33 -0700

    Update unit2&3project.py

--- unit2&3project.py
+++ unit2&3project.py
@@ -4,14 +4,14 @@ Date: 10/22/24
 Assignment: Unit 2 and 3 Project
 '''
 
-#spring = march 20
-#summer = june 21
-#fall = september 22
-#winter = december 21
+#spring: march 20
+#summer: june 21
+#fall: september 22
+#winter: december 21
 
 user_month = input("Enter the name of the month: ")
 month = user_month.title()
-day = int(input("Enter the day: "))
+day = int(input("Enter the day (1-31): "))
 
 if month == "March" and day >= 20 or month == "April" or month == "May" or month == "June" and day < 21:
     print(f"{month} {day} is in Spring")
